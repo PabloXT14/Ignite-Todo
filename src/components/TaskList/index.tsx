80a8b90337bb88bd39561234b1fdcf6406c1b883
Pablo Alan <pabloxt14@gmail.com>
2022-06-22 18:08:18 -0300

    feat: implemented warning container

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,5 +1,7 @@
 import { PlusCircle } from 'phosphor-react';
 import { FormEvent } from 'react';
+import Clipboard from '../../assets/Clipboard.svg';
+
 import styles from './styles.module.css';
 
 export function TaskList() {
@@ -17,6 +19,27 @@ export function TaskList() {
                     Criar <PlusCircle size={20} />
                 </button>
             </form>
+
+            <div className={styles.tasksContainer}>
+                <div className={styles.tasksInfo}>
+                    <div className={styles.numberOfTasks}>
+                        Tarefas criadas<span className={styles.indicator}>0</span>
+                    </div>
+                    <div className={styles.numberOfTasksFinished}>
+                        Concluídas<span className={styles.indicator}>0</span>
+                    </div>
+                </div>
+
+                <div className={styles.warningContainer}>
+                    <img src={Clipboard} alt="Imagem de caderneta" />
+                    <strong>Você ainda não tem tarefas cadastradas</strong>
+                    <span>Crie tarefas e organize seus itens a fazer</span>
+                </div>
+
+                <div className={styles.listTasks}>
+
+                </div>
+            </div>
         </div>
     )
 }
