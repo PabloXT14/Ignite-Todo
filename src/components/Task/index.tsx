844e29e4e88890b60b9cdf95f16b99fcda162607
Pablo Alan <pabloxt14@gmail.com>
2022-06-28 18:12:41 -0300

    feat: styling checkbox

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,14 +1,20 @@
 import { Trash } from 'phosphor-react';
 import styles from './styles.module.css';
+import { v4 as uuidv4 } from 'uuid';
 
 export function Task() {
+    const tempId = uuidv4();
+
     return (
         <div className={styles.container}>
-            <input type="checkbox" />
+            <div className={styles.checkboxContainer}>
+                <input type="checkbox" id={tempId} />
+                <label htmlFor={tempId}></label>
+            </div>
             <p className={styles.content}>
                 Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
             </p>
-            <Trash size={24}/>
+            <Trash size={20}/>
         </div>
     );
 }
